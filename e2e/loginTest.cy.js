4b4b010eadd61b7b90addd48a77c769d200eb7b4
Ray Cakradiningrat <50711922+raycakraa@users.noreply.github.com>
2025-05-15 00:03:04 +0700

    Update loginTest.cy.js

--- e2e/loginTest.cy.js
+++ e2e/loginTest.cy.js
@@ -3,6 +3,11 @@ import LoginPage from './loginPage';
 describe('Login Test on OrangeHRM', () => {
   const loginPage = new LoginPage();
 
+  it('should display all elements on the login page', () => {
+        loginPage.visit();
+        loginPage.assertLoginPageElements();
+    });
+
   it('should login successfully with valid credentials', () => {
     loginPage.visit();
     loginPage.enterUsername('Admin');
