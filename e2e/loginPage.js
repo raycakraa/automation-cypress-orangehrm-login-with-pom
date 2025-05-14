abfc2d0f35408d11ac83b0588691f61a08ab2c22
Ray Cakradiningrat <50711922+raycakraa@users.noreply.github.com>
2025-05-15 00:02:47 +0700

    Update loginPage.js

--- e2e/loginPage.js
+++ e2e/loginPage.js
@@ -3,6 +3,30 @@ class LoginPage {
     cy.visit('https://opensource-demo.orangehrmlive.com/');
   }
 
+  getUsernameField() {
+        return cy.get('input[name="username"]', { timeout: 5000 });
+    }
+
+    getPasswordField() {
+        return cy.get('input[name="password"]', { timeout: 5000 });
+    }
+
+    getLoginButton() {
+        return cy.get('button[type="submit"]');
+    }
+
+    getLogo() {
+        return cy.get('.orangehrm-login-branding > img');
+    }
+
+    getForgotPasswordLink() {
+        return cy.get('.orangehrm-login-forgot-header');
+    }
+
+    getCopyright() {
+        return cy.get('.orangehrm-copyright');
+    }
+
   enterUsername(username) {
     cy.get('input[name="username"]').type(username);
   }
@@ -18,6 +42,15 @@ class LoginPage {
   assertLoginSuccess() {
     cy.get('h6').should('contain.text', 'Dashboard');
   }
+
+  assertLoginPageElements() {
+        this.getUsernameField().should('be.visible');
+        this.getPasswordField().should('be.visible');
+        this.getLoginButton().should('be.visible');
+        this.getLogo().should('be.visible');
+        this.getForgotPasswordLink().should('be.visible').and('contain.text', 'Forgot your password?');
+        this.getCopyright().should('be.visible').and('contain.text', 'OrangeHRM, Inc.');
+    }
 }
 
 export default LoginPage;
